a35db8d8869715e69f0597e9c2d9dd77ac57471f
Sush1sui <jplovesraw@gmail.com>
2024-11-08 21:24:02 +0800

    fixed bug

--- src/events/autoReact.ts
+++ src/events/autoReact.ts
@@ -3,17 +3,21 @@ import { Message } from "discord.js";
 const CHANNELS = ["1140719367366123541"];
 
 export default {
-    name: "messageReactionAdd",
+    name: "messageCreate", // Correct event for when a message is sent
     once: false,
     async execute(message: Message) {
         try {
-            if (message.author.bot || !CHANNELS.includes(message.channel.id))
+            // Check if the message is from a bot or not in the specified channels
+            if (message.author.bot || !CHANNELS.includes(message.channel.id)) {
                 return;
-            await message.react("<a:40741rainbowheart:1287594223222128731>");
-            await message.react("<a:66082redcrystalheart:1287594201277661224>");
+            }
+
+            // React with custom animated emojis
+            await message.react("40741rainbowheart:1287594223222128731");
+            await message.react("66082redcrystalheart:1287594201277661224");
         } catch (error) {
             console.error(
-                `Failed to delete message: ${(error as Error).message}`
+                `Failed to add reaction: ${(error as Error).message}`
             );
         }
     },
